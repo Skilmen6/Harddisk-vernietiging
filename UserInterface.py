9ca17312e8c435d1d4ce8bd5760755f79f0bae5a
LeonH29 <61967229+LeonH29@users.noreply.github.com>
2020-03-13 09:14:23 +0100

    improved quit button and added pin movement.

--- UserInterface.py
+++ UserInterface.py
@@ -1,22 +1,33 @@
 from tkinter import *
 from PIL import Image, ImageTk
+from time import sleep
+import tkinter.messagebox
+#import pifacerelayplus #werkt alleen op Raspberry Pi
 root = Tk()
 
-#for testing on Raspberry Pi
-root.attributes('-fullscreen', True)
+#for testing on Raspberry Pi touch screen
+#root.attributes('-fullscreen', True)
 
-#for testing on Windows
-#root.geometry("800x480")
+#for testing on other screens
+root.geometry("800x480")
 
 imgFrame = Frame(root)
-buttonFrame = Frame(root, bg="grey")
+buttonFrame = Frame(root)
 
 img = Image.open("blank.png")
 img = img.resize((675, 405), Image.ANTIALIAS) 
-photoImg = ImageTk.PhotoImage(img) 
+photoImg = ImageTk.PhotoImage(img)
+#pinRelay = pifacerelayplus.PiFaceRelayPlus(pifacerelayplus.RELAY)
 
-def close_window(): 
-    root.destroy()
+def close_window():
+    MsgBox = tkinter.messagebox.askquestion ('Afsluiten','Weet je zeker dat je het programma wil sluiten?',icon = 'warning')
+    if MsgBox == 'yes':
+       root.destroy()
+
+#def move_pin():
+#    pinRelay.relays[0].toggle() #pin naar beneden
+#    sleep(3.0)
+#    pinRelay.relays[0].toggle() #pin naar boven
 
 
 imgPhoto = Label(
@@ -30,13 +41,16 @@ btnYes = Button(
     width=20, 
     height=4,
     bg="green", #bg = background
-    fg="white" #fg = foreground(text)
+    fg="white", #fg = foreground(text)
+    #command=move_pin
 )
 
 btnQuit = Button(
     buttonFrame,
     text="Quit",
     fg="red",
+    width=10, 
+    height=2,
     command=close_window
 )
 
@@ -54,7 +68,7 @@ buttonFrame.pack(side=BOTTOM, fill="x")
 
 imgPhoto.pack()
 btnYes.pack(side=LEFT, anchor="w")
-btnQuit.place(relx=0.5, rely=0.5, anchor="c")
+btnQuit.place(relx=0.5, rely=1, anchor="s")
 btnNo.pack(side=RIGHT, anchor="e")
 
 root.mainloop()
